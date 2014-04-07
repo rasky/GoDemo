9e79bee287f2f4d27b56b17ffbc27c6735e4c4fc
Mohamed Machkour <mad@develer.com>
2014-04-07 17:54:16 +0200

    add comments

--- GoDemo.go
+++ GoDemo.go
@@ -1,3 +1,9 @@
+/*
+  GoDemo implements a simple TCP demon that decode
+  an ans.1 DER formt, and output the decoded  data on
+  stdout.
+*/
+
 package main
 
 
@@ -16,11 +22,14 @@ type Result struct {
 	Exponent int
 }
 
-
+// connHander it is the called
+// goroutine that serve the user requests.
 func connHandler(cn net.Conn) {
 
+	// alloc buffer for incoming data on socket
 	encodeData := new(bytes.Buffer)
 
+	// data is a simple slice to collect partial chunks
 	data := make([]byte, 512)
 
 	for {
@@ -36,24 +45,32 @@ func connHandler(cn net.Conn) {
 
 	}
 
+	// structred data to store decoded data
 	var decData Result
 
+	// compute unmarchal function
 	_, err := asn1.Unmarshal(encodeData.Bytes(), &decData)
 
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
+	// render decoded data on stdout
 
 	fmt.Print("Decoded\nINTEGER\t\t:")
+	// print modulus
 	for _, v := range decData.Modulus.Bytes() {
 		fmt.Printf("%X", v)
 	}
 
+	// render exponent
 	fmt.Print("\nINTEGER\t\t")
 	fmt.Printf(":%X\n", decData.Exponent)
 
 	fmt.Println();
+
+	// close connection to notify client
 	cn.Close();
 }
 
@@ -70,13 +87,14 @@ func main() {
 	defer ln.Close()
 
 	for {
-
+		// wait for incaming connection
 		conn, err := ln.Accept()
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		// serve request
 		go connHandler(conn)
 	}
 }
